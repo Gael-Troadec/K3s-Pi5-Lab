61af0847dbe39ecc465f8b0d2df7d8f195544cb2
Troadec <troadec.gael.photography@gmail.com>
2025-11-19 09:20:54 +0100

    fix: force print flush python

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import time
 import platform
 
-print("Demarrage du systeme Architeuthis...")
-print(f"Architeuthis detectee : {platform.machine()}")
+print("Demarrage du systeme Architeuthis...", flush=True)
+print(f"Architeuthis detectee : {platform.machine()}", flush=True)
 
 while True:
-    print("En attente d'ordres...")
+    print("En attente d'ordres...", flush=True)
     time.sleep(60)
